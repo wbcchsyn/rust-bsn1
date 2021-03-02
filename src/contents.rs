d9d2305d3fa43f42e0b2bf3db4ea0cad80258582
Yoshida Shin <wbcchsyn@gmail.com>
2021-03-02 13:26:04 +0900

    Create function 'contents::to_bool_der()'

--- src/contents.rs
+++ src/contents.rs
@@ -170,6 +170,24 @@ pub fn to_bool_ber(bytes: &[u8]) -> Result<bool, Error> {
     }
 }
 
+/// Parses `bytes` as 'ASN.1 DER contents.'
+///
+/// This function is valid only for the contents of 'DER', and not applied to the contents of
+/// 'CER' nor 'BER.'
+pub fn to_bool_der(bytes: &[u8]) -> Result<bool, Error> {
+    if bytes.is_empty() {
+        Err(Error::UnTerminatedBytes)
+    } else if 1 < bytes.len() {
+        Err(Error::InvalidContents)
+    } else if bytes[0] == 0x00 {
+        Ok(false)
+    } else if bytes[0] == 0xff {
+        Ok(true)
+    } else {
+        Err(Error::InvalidContents)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
