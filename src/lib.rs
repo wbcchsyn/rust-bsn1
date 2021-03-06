a258facbbc6259924fb130d9e50e6cda93b3a551
Yoshida Shin <wbcchsyn@gmail.com>
2021-03-06 14:25:50 +0900

    Crate document

--- src/lib.rs
+++ src/lib.rs
@@ -53,24 +53,28 @@
 
 #![deny(missing_docs)]
 
-//! `x690` treats 'X.690' and 'ASN.1.'
+//! `x690` treats 'ASN.1.'
 //!
-//! # What is X.690 and ASN.1?
+//! # What is ASN.1?
 //!
-//! 'X.690' is an 'ITU-T' standard specifying several ASN.1 encording formats.
+//! ASN.1 stands for 'Abstruct Syntax Notation One' and X.690 is an 'ITU-T' standard specifying
+//! the following ASN.1 encording formats.
 //!
-//! 'ASN.1' stands for 'Abstruct Syntax Notation One' and 'X.690' is an 'ITU-T' standard specifying
-//! several 'ASN.1' encording formats.
+//! - Basic Encoding Rules (BER)
+//! - Canonical Encoding Rules (CER)
+//! - Distinguished Encoding Rules (DER)
 //!
-//! 'ASN.1' resembles 'JSON' in some ways, because they both are about serializing structured data,
-//! however, they differs in the following points.
+//! This crate supports BER and DER so far.
 //!
-//! - 'JSON' are easy for human to read, on the other hand, 'ASN.1' is readable for a computer.
-//!   i.e. 'ASN.1' consumes less computer resources and less CPU time than 'JSON'.
-//! - 'ASN.1' treats 'Integer', 'Boolean', 'String', 'Sequence', and so on like 'JSON'. What is
-//!    more, 'ASN.1' allows for users to define a new data type.
+//! ASN.1 formats resembles 'JSON' in some ways, because they both are about serializing
+//! structured data, however, they differs in the following points.
 //!
-//! 'ASN.1' has been used all over the world for a long time and very stable. For example,
+//! - JSON are easy for human to read, on the other hand, ASN.1 is readable for a computer.
+//!   i.e. ASN.1 consumes less computer resources and less CPU time than JSON does.
+//! - ASN.1 defines types 'Integer', 'Boolean', 'String', 'Sequence', and so on universally like
+//!   JSON. What is more, ASN.1 allows for users to define a new data type.
+//!
+//! ASN.1 has been used all over the world for a long time and very stable. For example,
 //! 'Transport Layer Security (TLS, SSL)', 'Lightweight Directory Access Protocol (LDAP)',
 //! '4th Generation Mobile Communication System (4G)', and so on.
 //!
@@ -83,8 +87,8 @@
 //! `x690` treats user defined data as well as universal data types; while most other rust
 //! crates can treat only universal data.
 //!
-//! To be accurate, there are 4 classes in 'ASN.1;' universal class, application class, context
-//! specific class, and private class. `bns1` knows all of them.
+//! To be accurate, there are 4 classes in ASN.1; universal class, application class, context
+//! specific class, and private class. `x690` knows all of them.
 
 mod ber;
 mod buffer;
