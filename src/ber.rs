615fe39be1a38c815175961d15ebcd67fb3c6f0c
Yoshida Shin <wbcchsyn@gmail.com>
2022-03-28 16:03:23 +0900

    Create function 'Ber::from_bytes_starts_with_unchecked()'

--- src/ber.rs
+++ src/ber.rs
@@ -441,6 +441,52 @@ impl Ber {
         }
     }
 
+    /// Builds a new instance from `bytes` that starts with 'BER' octets.
+    ///
+    /// `bytes` may include some extra octet(s) at the end.
+    ///
+    /// If it is not sure whether `bytes` starts with DER octets or not, use [`TryFrom`]
+    /// implementation or [`from_bytes`] .
+    ///
+    /// The difference from [`from_bytes_unchecked`] is that this function checks the 'LENGTH'
+    /// octets and excludes extra octet(s) at the end if any, while [`from_bytes_unchecked`]
+    /// does not check at all.
+    ///
+    /// # Safety
+    ///
+    /// The behavior is undefined if `bytes` does not start with 'ASN.1 BER' octets.
+    ///
+    /// [`TryFrom`]: #impl-TryFrom%3C%26%27_%20%5Bu8%5D%3E
+    /// [`from_bytes_unchecked`]: #method.from_bytes_unchecked
+    /// [`from_bytes`]: #method.from_bytes
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use bsn1::{Ber, IdRef};
+    ///
+    /// let ber0 = Ber::new(IdRef::octet_string(), &[]);
+    /// let mut bytes = Vec::from(ber0.as_ref() as &[u8]);
+    /// bytes.extend(&[1, 2, 3]);
+    ///
+    /// let ber1 = unsafe { Ber::from_bytes_starts_with_unchecked(bytes.as_ref()) };
+    /// assert_eq!(ber0, ber1);
+    /// ```
+    #[inline]
+    pub unsafe fn from_bytes_starts_with_unchecked(bytes: &[u8]) -> Self {
+        let id = identifier::shrink_to_fit_unchecked(bytes);
+        let parsing = &bytes[id.len()..];
+
+        let (len, parsing) = match length::from_bytes_starts_with_unchecked(parsing) {
+            (Length::Definite(len), parsing) => (len, parsing),
+            _ => panic!("{}", Error::IndefiniteLength),
+        };
+
+        let total_len = bytes.len() - parsing.len() + len;
+        let bytes = &bytes[..total_len];
+        Self::from_bytes_unchecked(bytes)
+    }
+
     /// Creates a new instance from `id` and `contents` .
     ///
     /// # Examples
