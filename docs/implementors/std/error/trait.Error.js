7a19de1e7e9de4f18ee60537bc58e4f5383313d9
Yoshida Shin <wbcchsyn@gmail.com>
2022-12-01 17:56:37 +0900

    Regenerate the document

--- docs/implementors/std/error/trait.Error.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {};
-implementors["bsn1"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.58.1/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"enum\" href=\"bsn1/enum.Error.html\" title=\"enum bsn1::Error\">Error</a>","synthetic":false,"types":["bsn1::Error"]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- docs/source-files.js
+++ docs/source-files.js
@@ -1,3 +1,11 @@
-var N = null;var sourcesIndex = {};
-sourcesIndex["bsn1"] = {"name":"","files":["ber.rs","buffer.rs","contents.rs","der.rs","identifier.rs","length.rs","lib.rs"]};
+var sourcesIndex = JSON.parse('{\
+"bsn1":["",[],["ber.rs","buffer.rs","contents.rs","contents_ref.rs","der.rs","id_tags.rs","identifier.rs","length.rs","lib.rs","stack_buffer.rs"]],\
+"num":["",[],["lib.rs"]],\
+"num_bigint":["",[["bigint",[],["addition.rs","bits.rs","convert.rs","division.rs","multiplication.rs","power.rs","shift.rs","subtraction.rs"]],["biguint",[],["addition.rs","bits.rs","convert.rs","division.rs","iter.rs","monty.rs","multiplication.rs","power.rs","shift.rs","subtraction.rs"]]],["bigint.rs","biguint.rs","lib.rs","macros.rs"]],\
+"num_complex":["",[],["cast.rs","complex_float.rs","lib.rs","pow.rs"]],\
+"num_integer":["",[],["average.rs","lib.rs","roots.rs"]],\
+"num_iter":["",[],["lib.rs"]],\
+"num_rational":["",[],["lib.rs","pow.rs"]],\
+"num_traits":["",[["ops",[],["checked.rs","euclid.rs","inv.rs","mod.rs","mul_add.rs","overflowing.rs","saturating.rs","wrapping.rs"]]],["bounds.rs","cast.rs","float.rs","identities.rs","int.rs","lib.rs","macros.rs","pow.rs","real.rs","sign.rs"]]\
+}');
 createSourceSidebar();
