3376a5c6658bb94ddb79363dfc479107cd835da6
Yoshida Shin <wbcchsyn@gmail.com>
2023-11-20 19:36:30 +0900

    Create module bsn1_serde_macros::attribute

--- bsn1_serde_macros/src/attribute.rs
+++ bsn1_serde_macros/src/attribute.rs
@@ -29,8 +29,3 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
-#![deny(missing_docs)]
-#![doc = include_str!("../README.md")]
-
-mod attribute;
