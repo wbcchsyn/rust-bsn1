1f896f0e659754f6589c8d7e6ec023a9b1997e95
Yoshida Shin <wbcchsyn@gmail.com>
2023-12-31 19:57:30 +0900

    Create alias bsn1_serde::macro_alias::Error

--- bsn1_serde/src/macro_alias.rs
+++ bsn1_serde/src/macro_alias.rs
@@ -29,3 +29,5 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+pub use bsn1::Error;
