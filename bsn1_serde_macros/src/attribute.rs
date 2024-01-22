cf0193781a0ef0bfd01d250e959989c70a9ee57f
Yoshida Shin <wbcchsyn@gmail.com>
2024-01-22 14:43:26 +0900

    Create method `Attribute.sanitize_as_variant()`

--- bsn1_serde_macros/src/attribute.rs
+++ bsn1_serde_macros/src/attribute.rs
@@ -315,6 +315,31 @@ impl Attribute {
         self.sanitize_as_container()
     }
 
+    pub fn sanitize_as_variant(&self) -> syn::Result<()> {
+        macro_rules! not_allowed {
+            ($a: ident) => {
+                if let Some(ref a) = self.$a {
+                    error(
+                        &a.ident,
+                        format!("The arguemnt `{}` is not allowed for enum.", a.ident),
+                    )?;
+                }
+            };
+        }
+
+        not_allowed!(skip_serialzing);
+        not_allowed!(skip_deserialzing);
+        not_allowed!(skip);
+        not_allowed!(default);
+
+        not_allowed!(into);
+        not_allowed!(from);
+        not_allowed!(to);
+        not_allowed!(try_from);
+
+        self.sanitize()
+    }
+
     pub fn id(&self, default_id: u8) -> Option<TokenStream> {
         if self.id_.is_none()
             && self.tag_class.is_none()
